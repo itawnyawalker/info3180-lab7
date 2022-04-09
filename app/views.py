6398ed70c7248db31dad4e861e806aaca4d61774
Itawnya Walker <itawnya.walker@gmail.com>
2022-04-09 09:09:34 -0500

    Exercise 2 Complete

--- app/views.py
+++ app/views.py
@@ -7,7 +7,9 @@ This file creates your application.
 
 from app import app
 from flask import render_template, request, jsonify, send_file
+from werkzeug.utils import secure_filename
 import os
+from app.forms import UploadForm
 
 
 ###
@@ -17,6 +19,26 @@ import os
 @app.route('/')
 def index():
     return jsonify(message="This is the beginning of our API")
+
+@app.route('/api/upload', methods=['POST'])
+def upload():
+    """Upload file"""
+    form = UploadForm()
+    if request.method=='POST':
+        if form.validate_on_submit():
+            description = form.description.data
+            photo = form.photo.data
+            filename = secure_filename(photo.filename)
+            photo.save(os.path.join(app.config['UPLOAD_FOLDER'],filename))
+
+            upload= [{"message": "File upload succesfull", "filename": filename, "description": description}]
+
+            return jsonify(upload=upload),202
+        else:
+            errors = form_errors(form)
+            return jsonify(errors=errors)
+
+
 
 
 ###
